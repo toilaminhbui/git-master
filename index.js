2af9a9b441e3306cd28e2bfe5c4c8405023d0d8e
toilaminhbui <toilaminhbui@gmail.com>
2021-09-02 10:10:12 +0700

    update api new

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const { connectDB } = require("./db")
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
+const routerPost = require("./routers/post")
+const routerUser = require("./routers/user")
+const routerAuth = require("./routers/auth")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.get("/", (req, res) => {
   res.json({ status: "success", message: "Bùi Văn Minh nha bà con" })
 })
+app.use("/api/post", routerPost)
+app.use("/api/user", routerUser)
+app.use("/api/auth", routerAuth)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
