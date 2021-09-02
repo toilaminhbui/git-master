300397b36056cd36632ab78771f5b9ad4939683d
toilaminhbui <toilaminhbui@gmail.com>
2021-09-02 21:33:31 +0700

    cahnge db

--- db.js
+++ db.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true
-    })
+    await mongoose.connect(
+      process.env.DB_URI ||
+        "mongodb+srv://admin-db:rFOo35aljQqsc7db@cluster0.f3ryp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true
+      }
+    )
     console.log("👌 mongoose is connected")
   } catch (err) {
     console.log("🚫", err)
