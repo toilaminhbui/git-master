018832310bc275199769c498b92fca4348e72d51
toilaminhbui <toilaminhbui@gmail.com>
2021-09-02 10:38:53 +0700

    change db

--- db.js
+++ db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://admin-db:rFOo35aljQqsc7db@cluster0.f3ryp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true
-      }
+      "mongodb+srv://admin-db:rFOo35aljQqsc7db@cluster0.f3ryp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     )
     console.log("👌 mongoose is connected")
   } catch (err) {
